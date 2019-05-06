c95cb47ac882096f516b4e0917de1653cb4302fa
Akshay C <nairakshay77@gmail.com>
2019-05-06 19:17:14 +0530

    first commit
    basic implementation. downloads the data 2000 bytes a time

---
+++ downloader.go
@@ -0,0 +1,135 @@
+package main
+
+import "net/http"
+import "log"
+import "io/ioutil"
+import "os"
+import "fmt"
+import "strconv"
+
+var (
+	contentChannel  = make(chan Content, channelNumber)
+	complete        = make(chan bool, downloaderCount)
+	writerComplete  = make(chan bool, fileWriter)
+	done            = false
+	channelNumber   = 20
+	fileWriter      = 4
+	downloaderCount = 4
+)
+
+type Content struct {
+	Data     []byte
+	Location int64
+}
+
+func getRangeEnd(start, end, interval int64) int64 {
+
+	if start+interval > end {
+		return end
+	}
+
+	return start + interval
+}
+
+//function to download the content and add it to the channel
+func downloader(start, end, interval int64, url string) {
+	client := &http.Client{}
+
+	for start < end {
+		req, _ := http.NewRequest("GET", url, nil)
+
+		rangeEnd := getRangeEnd(start, end, interval)
+		log.Printf("range data : %d - %d  (%d)\n", start, rangeEnd, end)
+		req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, rangeEnd))
+
+		res, _ := client.Do(req)
+
+		if res.StatusCode != 206 {
+
+			log.Println("Got Response Code : ", res.StatusCode)
+
+		} else {
+			body, _ := ioutil.ReadAll(res.Body)
+			contentChannel <- Content{Data: body, Location: start}
+			start = rangeEnd + 1
+		}
+
+	}
+
+	complete <- true
+
+}
+
+//function to write the content from channel into the file
+func writerFunc(file *os.File) {
+	//need to add the logic to the function to stop the function when all the content have been downloaded and written into the file
+	var data Content
+
+	for len(complete) != downloaderCount {
+		select {
+		case data = <-contentChannel:
+			file.WriteAt(data.Data, data.Location)
+		}
+	}
+
+	writerComplete <- true
+}
+
+func main() {
+	var i int
+	var start, end int64
+	var fileName = "downloadedFile" //default file name
+
+	if len(os.Args) < 2 {
+		log.Println("Download URL not Provided")
+		os.Exit(0)
+	}
+
+	url := os.Args[1]
+
+	if len(os.Args) > 2 {
+		fileName = os.Args[2]
+	}
+
+	log.Println("started data download...")
+
+	resp, err := http.Head(url)
+	if err != nil {
+		log.Println("Following error occured while requesting the server : ", err.Error())
+		os.Exit(0)
+	}
+
+	length := resp.Header.Get("Content-Length")
+	contentLen, err := strconv.ParseInt(length, 10, 64)
+	if err != nil {
+		log.Println("Following fetching Content-Length : ", err.Error())
+		os.Exit(0)
+	}
+
+	file, _ := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0755)
+
+	start = 0
+	end = -1
+
+	for i < channelNumber {
+
+		start = end + 1
+
+		if i == 3 {
+			end = contentLen
+		} else {
+			end = int64((contentLen / int64(channelNumber)) * (int64(i) + 1))
+		}
+
+		go downloader(start, end, int64(2000), url) //20 byte is default interval
+		go writerFunc(file)
+
+		i += 1
+	}
+	for len(writerComplete) != fileWriter {
+	}
+
+	log.Println("file writing complete")
+	file.Close()
+
+}
